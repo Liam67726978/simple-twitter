2f149c7115871d6f5d3bc21e877ecb49538b806a
Liam <liam67726978@gmail.com>
2023-06-06 08:00:47 +0800

    refactor: move Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
-import Layout from './layout/Layout'
 import LoginPage from './pages/LoginPage'
 
 export default function App() {
-  return (
-    <Layout className='justify-content-center'>
-      <LoginPage />
-    </Layout>
-  )
+  return <LoginPage />
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,10 @@
 import Login from '../components/Login'
+import Layout from '../layout/Layout'
 
 export default function LoginPage() {
-  return <Login />
+  return (
+    <Layout className='justify-content-center'>
+      <Login />
+    </Layout>
+  )
 }
