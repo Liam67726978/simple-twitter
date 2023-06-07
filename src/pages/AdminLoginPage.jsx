aa2cd80c014d48d56a7f9fbfcea64f3579fd40f4
Liam <liam67726978@gmail.com>
2023-06-08 01:08:28 +0800

    refactor: create AdminLogin component

--- src/pages/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage.jsx
@@ -1,3 +1,15 @@
-export default function adminLoginPage() {
-  return <div>adminLoginPage</div>
+import styled from 'styled-components'
+import Layout from '../layout/Layouts'
+import AdminLogin from '../components/AdminLogin'
+
+const StyledLayout = styled(Layout)`
+  padding-top: 64px;
+`
+
+export default function RegistPage() {
+  return (
+    <StyledLayout className='justify-content-center'>
+      <AdminLogin />
+    </StyledLayout>
+  )
 }
