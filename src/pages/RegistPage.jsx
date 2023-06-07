078000343d0bb09c43913418b12abea19a779bbc
Liam <liam67726978@gmail.com>
2023-06-08 00:42:04 +0800

    refactor: create Regist component

--- src/pages/RegistPage.jsx
+++ src/pages/RegistPage.jsx
@@ -1,3 +1,15 @@
+import styled from 'styled-components'
+import Layout from '../layout/Layouts'
+import Regist from '../components/Regist'
+
+const StyledLayout = styled(Layout)`
+  padding-top: 64px;
+`
+
 export default function RegistPage() {
-  return <div>RegistPage</div>
+  return (
+    <StyledLayout className='justify-content-center'>
+      <Regist />
+    </StyledLayout>
+  )
 }
