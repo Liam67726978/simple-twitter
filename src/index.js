4e891bec37ffb073a16b42799db1d4840ad683f6
Liam <liam67726978@gmail.com>
2023-06-18 23:48:15 +0800

    fix: fix basename url

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from './App'
 import './reset.css'
 import './index.scss'
 
-const basename = process.env.PUBLIC_URL
+const basename = process.env.REACT_APP_PUBLIC_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
       <App />
-     </BrowserRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
